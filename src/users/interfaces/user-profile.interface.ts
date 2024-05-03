fd95c6f932f9ab91652c92ce68999adfd12faba6
roomey_rahman <roomeyrahman@gmail.com>
2024-05-03 22:13:03 +0600

    add user interfaces

---
+++ src/users/interfaces/user-profile.interface.ts
@@ -0,0 +1,19 @@
+import { IMedia, IMobile } from '../../common/interfaces';
+import { IBase, ISocial } from '../../common/interfaces'
+
+export interface IUserProfile extends IBase {
+  readonly _id?: string;
+  readonly user?: string;
+  readonly firstName?: string;
+  readonly middleName?: string;
+  readonly lastName?: string;
+  readonly bio?: string;
+  readonly dob?: number;
+  readonly gender?: string;
+  readonly mobile?: IMobile;
+  readonly socials?: [ISocial];
+  readonly profilePic?: IMedia;
+  readonly coverPic?: IMedia;
+  readonly profilePercentage?: number;
+  readonly language?: string;
+}
