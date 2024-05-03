e4ff855d9bd10da8620053f839260546191ac22d
roomey_rahman <roomeyrahman@gmail.com>
2024-05-04 04:27:50 +0600

    add rent schema

---
+++ src/rents/schemas/rents.schema.ts
@@ -0,0 +1,102 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, SchemaTypes } from 'mongoose';
+import { UserDocument } from '../../users/schemas';
+import { SCHEMA } from '../../common/mock';
+import { Base } from '../../common/schemas';
+import { CarDocument } from '../../cars/schemas';
+import { TypeDocument } from '../../types/schemas';
+
+export type RentDocument = Rent & Document;
+
+@Schema({
+    toJSON: { virtuals: true, getters: true },
+    toObject: { virtuals: true, getters: true },
+})
+export class Rent extends Base {
+    @Prop({
+        type: SchemaTypes.ObjectId,
+        ref: SCHEMA.TYPE,
+        required: true,
+    })
+    carType: TypeDocument;
+
+    @Prop({
+        type: SchemaTypes.ObjectId,
+        ref: SCHEMA.CAR,
+        required: true,
+    })
+    car: CarDocument;
+
+    @Prop({
+        type: SchemaTypes.ObjectId,
+        ref: SCHEMA.USER,
+        required: true,
+    })
+    owner: UserDocument;
+
+    @Prop({
+        type: SchemaTypes.ObjectId,
+        ref: SCHEMA.USER,
+        required: true,
+    })
+    customer: UserDocument;
+
+    @Prop()
+    rentFromDate: number;
+
+    @Prop()
+    rentToDate: number;
+
+    @Prop()
+    pickup: string;
+
+    @Prop()
+    dropoff: string;
+
+    @Prop()
+    destiniation: string;
+
+    @Prop()
+    price: number;
+
+    @Prop({
+        type: SchemaTypes.Mixed, // Define as Mixed type for JSON object
+    })
+    priceBreakdown: Record<string, any>; // Define as Record<string, any> for JSON object
+
+    @Prop()
+    status: string;
+
+    @Prop()
+    paymentStatus: string;
+
+    @Prop()
+    notesForOwner: string;
+}
+
+export const RentSchema = SchemaFactory.createForClass(Rent);
+
+RentSchema.set('toJSON', {
+    transform: function (doc, ret) {
+        return {
+            _id: ret._id,
+            carType: ret.carType,
+            car: ret.car,
+            owner: ret.owner,
+            customer: ret.customer,
+            rentFromDate: ret.rentFromDate,
+            rentToDate: ret.rentToDate,
+            pickup: ret.pickup,
+            dropoff: ret.dropoff,
+            destiniation: ret.destiniation,
+            price: ret.price,
+            priceBreakdown: ret.priceBreakdown,
+            status: ret.status,
+            paymentStatus: ret.paymentStatus,
+            notesForOwner: ret.notesForOwner,
+            deliveryMethod: ret.deliveryMethod,
+            isActive: ret.isActive,
+            isDeleted: ret.isDeleted,
+        };
+    },
+});
