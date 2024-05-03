d50bbfe614b0e35a88aca4e039dfa98889ac50cb
roomey_rahman <roomeyrahman@gmail.com>
2024-05-03 22:01:48 +0600

    connect the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { HttpErrorFilter } from './common/filters';
 import { LoggingInterceptor, TransformInterceptor } from './common/interceptor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
+
+const DB_CONNECTION = process.env.DB_CONNECTION;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(DB_CONNECTION, {
+      autoIndex: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
