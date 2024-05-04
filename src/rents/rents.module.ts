c926573633e116e5340387fd4755d1bb1379a8eb
roomey_rahman <roomeyrahman@gmail.com>
2024-05-04 19:21:38 +0600

    make import schema in rent module

--- src/rents/rents.module.ts
+++ src/rents/rents.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RentsController } from './controllers/rents.controller';
 import { RentsService } from './services/rents.service';
+import { SCHEMA } from '../common/mock';
+import { RentSchema } from './schemas';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forFeature([
+      { name: SCHEMA.RENT, schema: RentSchema },
+    ]),
+  ],
   controllers: [RentsController],
   providers: [RentsService]
 })
